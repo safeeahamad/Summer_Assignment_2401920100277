c60da966f3ca24b5283c72a654e503d0682ae3f8
Safee Ahamad <safeeahamad6@gmail.com>
2026-06-25 11:02:59 +0530

    added day 4 solutions

---
+++ Week_4/Day_4/maxPathsum.java
@@ -0,0 +1,14 @@
+class Solution {
+    int maxSum = Integer.MIN_VALUE;
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int left = Math.max(0, dfs(root.left));
+        int right = Math.max(0, dfs(root.right));
+        maxSum = Math.max(maxSum, root.val+left+right);
+        return root.val + Math.max(left,right);
+    }
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return maxSum;
+    }
+}
